abfa7a80f8fd984cfd7d445cdc12cb0f5458f630
attachingmonster <3550462318@qq.com>
2019-04-23 20:56:18 +0800

    在SysUser里加注释，在Button_Click事件里加注释

--- WpfUserOrRoleManager/WpfUserOrRoleManager/MainWindow.xaml.cs
+++ WpfUserOrRoleManager/WpfUserOrRoleManager/MainWindow.xaml.cs
@@ -29,17 +29,22 @@ namespace WpfUserOrRoleManager
         AccountContext db = new AccountContext();
         UnitOfWork unitOfWork = new UnitOfWork();
 
+        /// <summary>
+        /// 登陆按钮事件
+        /// </summary>
+        /// <param name="sender"></param>
+        /// <param name="e"></param>
         private void Button_Click(object sender, RoutedEventArgs e)
         {
-            var users = unitOfWork.SysUserRepository.Get();
-            var user = users.Where(s => s.Account == this.tbxUserName.Text && s.Password == this.pbxPassword.Password).FirstOrDefault();
-            if (user == null)
+            var users = unitOfWork.SysUserRepository.Get();//获得用户表数据
+            var user = users.Where(s => s.Account == this.tbxUserName.Text && s.Password == this.pbxPassword.Password).FirstOrDefault();//与账号密码一致的用户
+            if (user != null)
             {
-                MessageBox.Show("用户和密码出错");
+                MessageBox.Show("登入成功!");
             }
             else
             {
-                MessageBox.Show("登入成功");
+                MessageBox.Show("用户和密码出错!");
             }
 
         }
--- WpfUserOrRoleManager/WpfUserOrRoleManager/Models/SysUser.cs
+++ WpfUserOrRoleManager/WpfUserOrRoleManager/Models/SysUser.cs
@@ -6,10 +6,19 @@ using System.Threading.Tasks;
 
 namespace WpfUserOrRoleManager.Models
 {
+    /// <summary>
+    /// 用户表
+    /// </summary>
     public class SysUser
     {
         public int ID { get; set; }
-        public string Account { get; set; }     
+        /// <summary>
+        /// 用户账号
+        /// </summary>
+        public string Account { get; set; }
+        /// <summary>
+        /// 密码
+        /// </summary>
         public string Password { get; set; }      
     }
 }
